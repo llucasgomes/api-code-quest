07445de32134c1aec0e4170c99be1329d5a3dfeb
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2026-07-21 18:36:45 -0400

    feature: add  Scalar
    - Adcionado o Scalar para melhorar a UI da Documentação
    - Foi feito a configuração de 'cors'

--- CodeQuest.API/Program.cs
+++ CodeQuest.API/Program.cs
@@ -1,3 +1,5 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,17 +8,34 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// configura para que as urls sejam minusculas
+builder.Services.AddRouting(option => option.LowercaseUrls = true);
+
+// Política de CORS para aceitar requisições do front end
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("FrontendPolicy", policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:3001", "https://localhost:3000", "http://localhost:3000", "https://localhost:3001")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference("/docs");
 }
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseCors("FrontendPolicy"); //CORS
 
 app.MapControllers();
 
